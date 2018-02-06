a5c78c2ade3b2aa7a9d0543737ba337433113e79
gerryATS <gerry@apptreetechnologies.com>
2018-02-06 18:04:48 +0530

    added the privacy text

--- resources/assets/js/components/PrivacyPolicy.vue
+++ resources/assets/js/components/PrivacyPolicy.vue
@@ -14,7 +14,62 @@
     <div class="container h-100 padding-top-lg padding-bottom-lg">
       <div class="row h-100">
         <div class="t_and_c_content padding-sm">
-          <h2>You have visited Go4SightSeeing's Privacy Policy page.</h2> 
+          <h2>Privacy Policy.</h2>
+          <p class="p1"><span class="s1">9711368 CANADA INC, Maplelabs, <a href="https://www.go4sightseeing.com"><span class="s2">go4sightseeing.com</span></a> (&lsquo;go4sightseeing&rsquo;, 'we', 'us', 'our') are committed to protecting and respecting your personal data privacy and complying with data protection principles and provisions under the Personal Data (Privacy) Ordinance (the 'Ordinance&rsquo;).</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1">We may collect, process, use and disclose your information when you use this website (this 'Website') and the services offered by go4sightseeing or the third party operators (the'Operators') through this Website (the 'Services'). 'You' and 'your' when used in this Privacy Policy includes any person who accesses this Website or use the Services.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1">This Privacy Policy sets out the basis and terms upon which Go4sightseeing collects, processes, uses and/or discloses your information that is obtained from you when you access this Website and/or use the Services. Such information may include personal information relating to or linked to a specific individual such as name, residential address, telephone number and email address ('Personal Information').</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1">Please read this Privacy Policy carefully. By visiting this Website, you are consenting to the collection, processing, usage and disclosure of your Personal Information as set out in this Privacy Policy.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1"><strong>Scope of Terms</strong></span></p>
+<p class="p1"><span class="s1">Go4sightseeing reserves the right to update, amend or modify the terms of this Privacy Policy or any part of it without prior notice, and your continued access of this Website or use of the Services signifies your acceptance of the updated, amended or modified Privacy Policy. If you do not agree to all the terms and conditions in this Privacy Policy and/or any subsequent updates, amendments or modifications thereto, you must stop accessing or otherwise using this Website and the Services.</span></p>
+<p class="p1"><span class="s1">Accordingly, please visit this page if you wish to access and view the current version of this Privacy Policy.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1"><strong>Collection of Information</strong></span></p>
+<p class="p1"><span class="s1">We may collect Personal Information about you that you provide to us while using this Website and information about how you use this Website including when you open your user account ('User Account'), visit this Website or make reservations for any intended Services or using the Services.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1">1) Opening Your User Account</span></p>
+<p class="p1"><span class="s1">When you open with us a User Account or amend any information of your User Account, we may collect your Personal Information, such as your name, email address, username, password and telephone number.</span></p>
+<p class="p1"><span class="s1">2) Making Reservations for the Services or Using the Services</span></p>
+<p class="p1"><span class="s1">(a) When you visit this Website, make reservations for any intended Services or use the Services, we may collect and process certain information (which may contain your Personal Information or may contain non-personally identifiable information but nevertheless linked to your Personal Information) including but not limited to those set out below: Copies of correspondence (whether by e-mail, instant or personal messaging or otherwise) between you and us; and between youre and the Operators;</span></p>
+<p class="p1"><span class="s1">(b) Details of your usage of this Website (including traffic data, location data and length of user sessions);</span></p>
+<p class="p1"><span class="s1">(c) Feedback on and responses to surveys conducted by Go4sightseeing relating to the Services and newsletters which may be published, circulated or distributed by Go4sightseeing;</span></p>
+<p class="p1"><span class="s1">(d) Information automatically collected and stored in our server or the server of our third party services provider by using or accessing to this Website (including the log-in name and password for your User Account, your computers Internet Protocol (IP) address, browser type, browser information, pages visited, previous or subsequent sites visited).</span></p>
+<p class="p1"><span class="s1">3) For your payment safety and to avoid misuse of your credit card information, Go4sightseeing may ask for your credit card details and a copy of a government issued ID to verify your purchase after payment is complete. For privacy reasons, you will be asked to cover your ID number and your credit card number except for the last 4 digits.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1"><strong>Storage of Information</strong></span></p>
+<p class="p1"><span class="s1">The Personal Information and other data we collect from you may be transferred to, processed, and stored in our servers or the servers of our third party services providers.</span></p>
+<p class="p1"><span class="s1">Go4sightseeing will use reasonable endeavours to maintain appropriate physical, electronic and organisational procedures to ensure that your Personal Information and other data is treated securely and in accordance with this Privacy Policy, and to protect such data against unauthorized access or unauthorized alteration, disclosure or destruction of data.</span></p>
+<p class="p1"><span class="s1">Once we have received your information, we will use strict procedures and security features to try to prevent unauthorized access. Go4sightseeing does not give any representation, warranty or undertaking that the Personal Information you provide to us will be secure at all times, and to the extent Go4sightseeing has fulfilled its obligations under no circumstances shall Go4sightseeing be responsible for any losses, damages, costs and expenses which you may suffer or incur arising from unauthorised access to or use of your Personal Information.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1">All payment transactions carried out by us or our chosen third-party provider of payment processing services will be encrypted using online encryption technology. You are responsible for keeping your chosen password confidential and not to share your password with any third party.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1"><strong>Usage of Information</strong></span></p>
+<p class="p1"><span class="s1">Go4sightseeing will not sell or rent your Personal Information to third parties.</span></p>
+<p class="p1"><span class="s1">Go4sightseeing will use Personal Information and other data collected through this Website or when making purchases for the Services to create your User Account, to provide you with the Services, to continually improve this Website and the Services, and to contact you in relation to the Services.</span></p>
+<p class="p1"><span class="s1">The purpose of using your Personal Information or such other data is to achieve faster purchase requests, better customer support and timely notice of new Services and special offers.</span></p>
+<p class="p1"><span class="s1">From time to time, we may also make use of your Personal Information to contact you for feedback on your use of this Website, to assist us in improving this Website, or to offer special savings or promotions to you, where you have indicated your consent to receiving such communications. If you would prefer not to receive notices of special savings or promotions, you may simply opt-out from receiving them by replying to us through the hyperlink provided in these notices.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1"><strong>Disclosure of Information</strong></span></p>
+<p class="p1"><span class="s1">We may from time to time share and disclose your Personal Information and other data to third parties, some of whom may be located outside your home country. The circumstances under which such sharing and disclosure will take place may include without limitation, the following:</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1">1) To successfully complete your reservations or otherwise implement our Terms of Use.</span></p>
+<p class="p1"><span class="s1">2) If you are a visitor, to the relevant Operator in connection with a Services which you have made reservations for or intend to make reservations for.</span></p>
+<p class="p1"><span class="s1">3) If you are an Operator, to any visitor in connection with the Services you are offering.</span></p>
+<p class="p1"><span class="s1">4) To our third party service providers (including Google Analytics) which we engage for the performance of certain services on our behalf, such as web hosting services, data analysis, marketing, market research, and to otherwise provide you with customer service.</span></p>
+<p class="p1"><span class="s1">5) If and to the extent required by any applicable law, order of court or requests by any governmental authority to make such disclosure.</span></p>
+<p class="p1"><span class="s1">6) In case of a corporate transaction, in connection with the sale, merger, acquisition, or other corporate reorganization or restructuring of our corporation, your Personal Information may be disclosed, shared or transferred to the new controlling entity or its authorised third party for carrying on our business.</span></p>
+<p class="p1"><span class="s1">7) To our advisors, agencies or other parties concerned in order to protect the rights and property of Go4sightseeing.</span></p>
+<p class="p1"><span class="s1">8) In any other case, to any third parties with your prior written consent.</span></p>
+<p class="p1"><span class="s1">There may be links present on this Website which could result in you leaving this Websites and/or being taken to other third party websites. You should note that any Personal Information that you provide to these third party websites are not subject to this Privacy Policy, and Go4sightseeing is not liable for any losses, damages, costs or expenses which you may suffer or incur in connection with you providing or making available Personal Information or other data to such third party websites.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1"><strong>Data Access and Correction</strong></span></p>
+<p class="p1"><span class="s1">You may access and correct your Personal Information through this Website or make your data access or correction request by sending your request by email at support@go4sightseeing.com. When handling a data access or correction request, we have the right to check the identity of the requester to ensure that he/she is the person entitled to make the data access or correction request. A data protection log book is maintained as required under the Ordinance.</span></p>
+<p class="p2">&nbsp;</p>
+<p class="p1"><span class="s1"><strong>Enquiries</strong></span></p>
+<p class="p1"><span class="s1">If you have any questions about this Privacy Policy, please contact us by email at support@go4sightseeing.com.</span></p>
         </div>
       </div>
     </div>
